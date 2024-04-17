4e9c338771132e4fde5105b27877ea1114b7e03f
Mikaila Brown <mbr2201@wgu.edu>
2024-04-17 08:08:45 -0400

    update guarantee

--- src/components/Guarantee/GuaranteeContent.jsx
+++ src/components/Guarantee/GuaranteeContent.jsx
@@ -6,7 +6,7 @@ export default function GuaranteeContent() {
         <>
             <div className='guarantee-content-container'>
                 <div className='guarantee-title'>
-                    <h2>Our Results Are <br/><b className='bold-large'>GUARANTEED</b></h2>
+                    <h2>Over 95% <br/><b className='bold-large'>Success rate</b></h2>
                 </div>
                 <div className='guarantee-description'>
                     <p>No one likes surprises, especially when it comes to their business. That&apos;s why we provide you with a free consultation and guarantee that the results we predict are realistic for you final campaign.</p>
